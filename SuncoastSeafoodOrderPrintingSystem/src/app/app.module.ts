ff52ed199cd397b9a9e8377cd21aa0ffc2cf114e
stevenguyenBitBucket <13271900+stevenguyen18595@users.noreply.github.com>
2022-01-08 17:38:00 +1000

    add bootstrap to Suncoast Seafood

--- SuncoastSeafoodOrderPrintingSystem/src/app/app.module.ts
+++ SuncoastSeafoodOrderPrintingSystem/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { SuncoastItemComponent } from './suncoast-item/suncoast-item.component';
 import { OrderComponent } from './order/order.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
